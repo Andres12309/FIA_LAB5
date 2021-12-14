c20b79ecd0b4b5982e3e0e74eec1c947888a1fe2
Andres12309 <49722499+Andres12309@users.noreply.github.com>
2021-12-13 22:22:19 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+
+let trainedNet;
+
+function encode(arg) {
+    return arg.split('').map(x => (x.charCodeAt(0) / 256));
+}
+
+function processTrainingData(data) {
+    return data.map(d => {
+        return {
+            input: encode(d.input),
+            output: d.output
+        }
+    })
+}
+
+function train(data) {
+    let net = new brain.NeuralNetwork();
+    net.train(processTrainingData(data));
+    trainedNet = net.toFunction();
+};
+
+
+var tweet = null;
+
+//submit
+function sub() {
+    tweet = document.getElementById("prod").value;
+    console.log(tweet)
+    console.log(execute(tweet));
+};
+
+function execute(input) {
+    var result = null;
+    let results = trainedNet(encode(input));
+    console.log(results)
+    let output;
+    let certainty;
+    if (results.trump > results.kardashian) {
+        output = 'Donald Trump'
+        certainty = Math.floor(results.trump * 100)
+    } else {
+        output = 'Kim Kardashian'
+        certainty = Math.floor(results.kardashian * 100)
+    }
+
+    result = "I'm " + certainty + "% sure that tweet was written by " + output;
+    document.getElementById("resultado").setAttribute("value",result)
+    return "I'm " + certainty + "% sure that tweet was written by " + output;
+}
+
+train(trainingData);
+
